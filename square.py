72b8082dc31edc33697687672a0923d9144a5141
Frank <fglantz2019@chadwickschool.org>
2016-10-09 23:53:57 -0700

    add square

---
+++ square.py
@@ -0,0 +1,23 @@
+from Myro import*
+init ("sim")
+penDown()
+forward(1,1)
+#I tested penUp and wait() in those locations
+#but it didn't seem to do anything
+#penDown()
+#wait(1)
+turnLeft(2.45,.5)
+turnLeft(2.45,.5)
+#penDown()
+wait(1)
+forward(1,1)
+#wait(1)
+turnLeft(2.45,.5)
+turnLeft(2.45,.5)
+wait(1)
+forward(1,1)
+#wait(1)
+turnLeft(2.45,.5)
+turnLeft(2.45,.5)
+wait(1)
+forward(1,1)
