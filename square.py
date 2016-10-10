e026d320c84ab6ff9e3ddbcf74585ebe56912e1e
Frank <fglantz2019@chadwickschool.org>
2016-10-10 00:17:56 -0700

    add function of squareDraw

--- square.py
+++ square.py
@@ -1,23 +1,28 @@
-from Myro import*
+from Myro import *
 init ("sim")
-penDown()
-forward(1,1)
-#I tested penUp and wait() in those locations
-#but it didn't seem to do anything
-#penDown()
-#wait(1)
-turnLeft(2.45,.5)
-turnLeft(2.45,.5)
-#penDown()
-wait(1)
-forward(1,1)
-#wait(1)
-turnLeft(2.45,.5)
-turnLeft(2.45,.5)
-wait(1)
-forward(1,1)
-#wait(1)
-turnLeft(2.45,.5)
-turnLeft(2.45,.5)
-wait(1)
-forward(1,1)
+def squareDraw():
+    #contents of functions are indented
+    penDown()
+    forward(1,1)
+    #I tested penUp and wait() in those locations
+    #but it didn't seem to do anything
+    #penDown()
+    #wait(1)
+    turnLeft(2.45,.5)
+    turnLeft(2.45,.5)
+    #penDown()
+    wait(1)
+    forward(1,1)
+    #wait(1)
+    turnLeft(2.45,.5)
+    turnLeft(2.45,.5)
+    wait(1)
+    forward(1,1)
+    #wait(1)
+    turnLeft(2.45,.5)
+    turnLeft(2.45,.5)
+    wait(1)
+    forward(1,1)
+
+
+squareDraw()
